5fc625aba77f2880d0f8b2cfaf5abd16a6ddc16c
Allen Sarmiento <allensarmiento3@gmail.com>
2020-04-08 09:58:10 -0700

    Remove extra config from config file

--- config/config.js
+++ config/config.js
@@ -7,12 +7,9 @@ require('dotenv').config();
 
 module.exports = {
   port: process.env.PORT || 8080,
-  secretOrKey: process.env.SECRET_OR_KEY || 'secretOrKey',
   vonageNumber: process.env.VONAGE_NUMBER,
   vonageApiKey: process.env.VONAGE_API_KEY,
   vonageApiSecret: process.env.VONAGE_API_SECRET,
-  vonagePrivateKeyFilePath: process.env.PRIVATE_KEY_FILE_PATH,
-  vonageApplicationId: process.env.VONAGE_APPLICATION_ID,
   production: process.env.PRODUCTION || 'development'
 };
 
